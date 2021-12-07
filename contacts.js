815d4321ccf7709683e116ad101af39abc6906c6
Iliya Lunev <lunevig90@gmail.com>
2021-12-07 18:05:53 +0200

    finish

--- contacts.js
+++ contacts.js
@@ -1,25 +1,61 @@
-const { uuid } = require("uuidv4");
-const path = require("path");
-const fs = require("fs");
+const { uuid } = require('uuidv4');
+const path = require('path');
+const fs = require('fs').promises;
+const { stringify } = require('querystring');
 
-const contactsPath = path.resolve("./db/contacts.json");
+const contactsPath = path.resolve('./db/contacts.json');
 
-// Добавить функцию fs
+// Добавить функцию fs для чтения
 
-function listContacts() {
-  // ...твой код
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const result = JSON.parse(data);
+    console.table(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  try {
+    const result = await listContacts();
+    const contactSelected = result.find(item => item.id === String(contactId));
+
+    console.table(contactSelected);
+
+    return contactSelected;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  try {
+    const result = await listContacts();
+    const newContact = result.filter(
+      contact => contact.id !== String(contactId),
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(newContact));
+
+    return console.table(newContact);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  const newContact = { id: uuid(), name, email, phone };
+  try {
+    const contacts = await fs.readFile(contactsPath, 'utf8');
+    const contactsParsed = JSON.parse(contacts);
+    const newContactList = [...contactsParsed, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(newContactList), 'utf8');
+    return console.table(newContactList);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
